9caa8487153a7bdda4e85bfaffe7ae363af2b04f
efrenruizrubio <efren282@outlook.es>
2022-07-11 13:00:41 -0500

    first design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Movements from "./components/Movements";
+
+const data = [
+  {
+    title: "Conceptos",
+    items: [
+      "Este es el concepto 1",
+      "Este es el concepto 2",
+      "Este es el concepto 3",
+      "Este es el concepto 4",
+      "Este es el concepto 5",
+      "Este es el concepto 6",
+    ],
+  },
+  {
+    title: "Imágenes",
+    items: [
+      "Esta es la imagen 1",
+      "Esta es la imagen 2",
+      "Esta es la imagen 3",
+      "Esta es la imagen 4",
+      "Esta es la imagen 5",
+      "Esta es la imagen 6",
+    ],
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Movements data={data} />
       </header>
     </div>
   );
---
+++ src/components/Movements.js
@@ -0,0 +1,121 @@
+import React, { useState, useRef } from "react";
+
+function Movements({ data, movements }) {
+  const [movementsData, setMovementsData] = useState(movements);
+  const [list, setList] = useState(data);
+  const [dragging, setDragging] = useState(false);
+  const dragItem = useRef();
+  const dragNode = useRef();
+
+  const handleDragStart = (e, params) => {
+    dragItem.current = params;
+    dragNode.current = e.target;
+    dragNode.current.addEventListener("dragend", handleDragEnd);
+    setTimeout(() => {
+      setDragging(true);
+    }, 0);
+  };
+
+  const handleDragEnter = (e, params) => {
+    const currentItem = dragItem.current;
+    if (e.target !== dragNode.current) {
+      setList((oldList) => {
+        let newList = JSON.parse(JSON.stringify(oldList));
+        newList[params.groupI].items.splice(
+          params.itemI,
+          0,
+          newList[currentItem.groupI].items.splice(currentItem.itemI, 1)[0]
+        );
+        dragItem.current = params;
+        return newList;
+      });
+    }
+  };
+
+  const handleDragEnd = () => {
+    setDragging(false);
+    dragNode.current.removeEventListener("dragend", handleDragEnd);
+    dragItem.current = null;
+    dragNode.current = null;
+  };
+
+  const getStyles = (params) => {
+    const currentItem = dragItem.current;
+    if (
+      currentItem.groupI === params.groupI &&
+      currentItem.itemI === params.itemI
+    ) {
+      return "current dnd-item";
+    }
+    return "dnd-item";
+  };
+
+  return (
+    <>
+      <div className="main-container">
+        <span className="title">Corrientes filos&oacute;ficas</span>
+        <section className="movements-container">
+          <div className="card">
+            <span>1</span>
+          </div>
+          <div className="card">
+            <span>2</span>
+          </div>
+          <div className="card">
+            <span>3</span>
+          </div>
+          <div className="card">
+            <span>4</span>
+          </div>
+          <div className="card">
+            <span>5</span>
+          </div>
+          <div className="card">
+            <span>6</span>
+          </div>
+        </section>
+
+        <div className="drag-n-drop">
+          {list.map((group, groupI) => (
+            <div
+              key={group.title}
+              className="dnd-group"
+              onDragEnter={
+                dragging && !group.items.length
+                  ? (e) => {
+                      handleDragEnter(e, { groupI, itemI: 0 });
+                    }
+                  : null
+              }
+            >
+              <div className="group-title">{group.title}</div>
+              {group.items.map((item, itemI) => (
+                <div
+                  draggable
+                  onDragStart={(e) => {
+                    handleDragStart(e, { groupI, itemI });
+                  }}
+                  onDragEnter={
+                    dragging
+                      ? (e) => {
+                          handleDragEnter(e, { groupI, itemI });
+                        }
+                      : null
+                  }
+                  key={item}
+                  className={
+                    dragging ? getStyles({ groupI, itemI }) : "dnd-item"
+                  }
+                >
+                  {item}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Movements;
